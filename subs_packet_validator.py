97b8ccdccf12ce5a9a220e702f26fa1a950c6cc1
HeroSp2491 <sonichero.themodder@gmail.com>
2020-12-30 16:48:23 +0200

    Move static methods in validator

--- subs_packet_validator.py
+++ subs_packet_validator.py
@@ -1,44 +1,45 @@
 import subs_packet_abstract_validator
 from subs_packet import SubsPacket, Type, Status
 
+ANSWER_CODES = (Status.ANSWER_CORRECT, Status.ANSWER_VICTORY,
+                Status.ANSWER_INCORRECT, Status.ANSWER_FULL_SUB)
+
+ERROR_CODES = (Status.ERROR_CLOSED, Status.ERROR_UNEXPECTED,
+               Status.ERROR_ATTEMPT_NOT_IN_TURN, Status.ERROR_OUT_OF_RANGE)
+
+
+def is_ready_packet_valid(packet: SubsPacket):
+    return True
+
+
+def is_attempt_packet_valid(packet: SubsPacket):
+    return packet.x_coord is not None and packet.y_coord is not None
+
+
+def is_answer_packet_valid(packet: SubsPacket):
+    return \
+        packet.x_coord is not None and \
+        packet.y_coord is not None and \
+        packet.status in ANSWER_CODES
+
+
+def is_error_packet_valid(packet: SubsPacket):
+    return packet.status in ERROR_CODES
+
+
+PACKET_TYPE_TO_VALIDATOR = {
+    Type.READY: is_ready_packet_valid,
+    Type.ERROR: is_error_packet_valid,
+    Type.ATTEMPT: is_attempt_packet_valid,
+    Type.ANSWER: is_answer_packet_valid
+}
+
 
 class SubsPacketValidator(subs_packet_abstract_validator.SubsPacketAbstractValidator):
     """
     Validates Subs Protocol packets by checking their fields
     based on their specified version and type.
     """
-
-    ANSWER_CODES = (Status.ANSWER_CORRECT, Status.ANSWER_VICTORY,
-                    Status.ANSWER_INCORRECT, Status.ANSWER_FULL_SUB)
-
-    ERROR_CODES = (Status.ERROR_CLOSED, Status.ERROR_UNEXPECTED,
-                   Status.ERROR_ATTEMPT_NOT_IN_TURN, Status.ERROR_OUT_OF_RANGE)
-
-    @staticmethod
-    def is_ready_packet_valid(packet: SubsPacket):
-        return True
-
-    @staticmethod
-    def is_attempt_packet_valid(packet: SubsPacket):
-        return packet.x_coord is not None and packet.y_coord is not None
-
-    @staticmethod
-    def is_answer_packet_valid(packet: SubsPacket):
-        return \
-            packet.x_coord is not None and \
-            packet.y_coord is not None and \
-            packet.status in SubsPacketValidator.ANSWER_CODES
-
-    @staticmethod
-    def is_error_packet_valid(packet: SubsPacket):
-        return packet.status in SubsPacketValidator.ERROR_CODES
-
-    PACKET_TYPE_TO_VALIDATOR = {
-        Type.READY: is_ready_packet_valid,
-        Type.ERROR: is_error_packet_valid,
-        Type.ATTEMPT: is_attempt_packet_valid,
-        Type.ANSWER: is_answer_packet_valid
-    }
 
     def __init__(self, protocol_version: str):
         self.protocol_version = protocol_version
@@ -47,7 +48,7 @@ class SubsPacketValidator(subs_packet_abstract_validator.SubsPacketAbstractValid
         if packet.version != self.protocol_version:
             return False
 
-        packet_type_validator = self.PACKET_TYPE_TO_VALIDATOR.get(packet.packet_type)
+        packet_type_validator = PACKET_TYPE_TO_VALIDATOR.get(packet.packet_type)
         if packet_type_validator is None:
             return False
 
